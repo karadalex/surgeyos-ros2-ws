2c5a7c5ab1b7d5a9b4033f61063d49db4e000601
Alexios Karadimos <karadalex@gmail.com>
2026-02-18 20:04:57 +0200

    added missing launch file

---
+++ src/robot_description2/launch/display.launch.py
@@ -0,0 +1,62 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import SetEnvironmentVariable
+from launch.conditions import IfCondition
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    use_joint_state_publisher = LaunchConfiguration("use_joint_state_publisher")
+    use_rviz = LaunchConfiguration("use_rviz")
+    use_software_gl = LaunchConfiguration("use_software_gl")
+    model = LaunchConfiguration("model")
+
+    return LaunchDescription([
+        DeclareLaunchArgument("use_joint_state_publisher", default_value="true"),
+        DeclareLaunchArgument("use_rviz", default_value="true"),
+        DeclareLaunchArgument("use_software_gl", default_value="true"),
+        DeclareLaunchArgument(
+            "model",
+            default_value=PathJoinSubstitution([
+                FindPackageShare("robot_description2"),
+                "urdf",
+                "dual_roarm.urdf.xacro",
+            ]),
+        ),
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            output="screen",
+            parameters=[{
+                "robot_description": ParameterValue(
+                    Command([FindExecutable(name="xacro"), " ", model]),
+                    value_type=str,
+                ),
+            }],
+        ),
+        Node(
+            package="joint_state_publisher",
+            executable="joint_state_publisher",
+            condition=IfCondition(use_joint_state_publisher),
+            output="screen",
+        ),
+        SetEnvironmentVariable(
+            name="LIBGL_ALWAYS_SOFTWARE",
+            value="1",
+            condition=IfCondition(use_software_gl),
+        ),
+        SetEnvironmentVariable(
+            name="QT_XCB_GL_INTEGRATION",
+            value="none",
+            condition=IfCondition(use_software_gl),
+        ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            condition=IfCondition(use_rviz),
+            output="screen",
+        ),
+    ])
