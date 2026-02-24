e76b97925cde5f0693f6286ec7a1e955ca65608d
Alexios Karadimos <karadalex@gmail.com>
2026-02-24 21:34:30 +0200

    fixed robto_description1 discovery

--- src/robot_description1/launch/display.launch.py
+++ src/robot_description1/launch/display.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "model",
             default_value=PathJoinSubstitution([
-                FindPackageShare("robot_description"),
+                FindPackageShare("robot_description1"),
                 "urdf",
                 "robot.urdf.xacro",
             ]),
@@ -30,7 +30,7 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "rviz_config",
             default_value=PathJoinSubstitution([
-                FindPackageShare("robot_description"),
+                FindPackageShare("robot_description1"),
                 "rviz",
                 "view.rviz",
             ]),
@@ -40,7 +40,7 @@ def generate_launch_description():
             executable="robot_state_publisher",
             output="screen",
             parameters=[{
-                "robot_description": ParameterValue(
+                "robot_description1": ParameterValue(
                     Command([FindExecutable(name="xacro"), " ", model]),
                     value_type=str,
                 ),
